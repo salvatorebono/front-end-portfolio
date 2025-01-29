32077d2d6bcfc16e1d4c5602acff9aebcb420ad5
salvobono <salvatorebono2001@gmail.com>
2025-01-29 13:02:11 +0100

    first commit

---
+++ src/app/data.js
@@ -0,0 +1,26 @@
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/salvobono",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/salvobono",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/Salvatore_Bono_Resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
