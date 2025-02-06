12ec0d941561a366463d6cbda1ca399774e9dfdf
salvobono <salvatorebono2001@gmail.com>
2025-02-06 22:23:30 +0100

    add space-y-8-custom

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -7,7 +7,7 @@ export default function Contact() {
       <>
          <Image src={bg} alt="background-image" className="-z-50 fixed h-full top-0 left-0 w-full object-cover object-center opacity-25" />
 
-         <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
+         <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8-custom">
             <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
                <h1 className="text-accent font-semibold text-center text-4xl capitalize">summon the wizard</h1>
                <p className="text-center font-light text-sm xs:text-base">
