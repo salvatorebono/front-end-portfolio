994ce2cd0cfc1f69c8e2b0033b0996386566d9c6
salvobono <salvatorebono2001@gmail.com>
2025-02-07 19:14:56 +0100

    add Analytics from vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import FireFliesBackground from "@/components/FireFliesBackground";
 import Sound from "@/components/Sound";
+import { Analytics } from "@vercel/analytics/react";
 import clsx from "clsx";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({ children }) {
         <FireFliesBackground />
         <Sound />
         <div id="my-modal" />
+        <Analytics />
       </body>
     </html>
   );
